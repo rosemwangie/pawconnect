14f2de4ab0dc9cc444f9195efd853eeb050f1442
Rose <rosemwangidev@gmail.com>
2024-06-01 14:21:20 +0300

    Implemented the user service layer with user methods

--- src/main/java/com/bobo/pawConnect/model/Users.java
+++ src/main/java/com/bobo/pawConnect/model/Users.java
@@ -1,25 +1,62 @@
 package com.bobo.pawConnect.model;
 
 import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
-
 public class Users {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_sequence", allocationSize = 1)
     private Long id;
     private String username;
-    private Long password;
-    private Long  email;
+    private String password;
+    private String email;
 
+    // Default constructor
+    public Users() {}
 
-    public void setId(java.lang.Long id) {
+    // Parameterized constructor
+    public Users(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
     }
-    public java.lang.Long getId() {
-        return id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
